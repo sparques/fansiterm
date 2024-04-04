b1871d7c9aa072063665ff1b6476946fa6e112dd
Matthew Hiles <matthew.hiles@gmail.com>
2024-04-04 14:17:45 -0400

    add juliamono font

---
+++ tiles/juliamono/juliamono.go
@@ -0,0 +1,4 @@
+//go:generate go run -tags gentileset ../gentileset/main.go  -fontfile=JuliaMono-Light.ttf -hinting=full -pkg juliamono -var Regular8x16 -size 16
+//go:generate go run -tags gentileset ../gentileset/main.go  -fontfile=JuliaMono-Bold.ttf -hinting=full -pkg juliamono -var Bold8x16 -size 16
+
+package juliamono
